5f17dbd124502480ac34954931b5e0c63f366184
Andrius <e33rrorr@gmail.com>
2026-04-20 12:39:02 +0100

    A simple client that connects to the server

---
+++ client.py
@@ -0,0 +1,14 @@
+import socket
+
+# It determines the client's IPV and which Protocol it uses
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Enter the server's IP and Port number
+server_ip = "127.0.0.1"
+server_port = 9000
+
+# Connects to the server
+client.connect((server_ip, server_port))
+
+print("Connected Successfully")
+
