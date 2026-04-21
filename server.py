74b01954b6656a3667d40bbc2d36c2203b342f41
Andrius <e33rrorr@gmail.com>
2026-04-21 20:14:12 +0100

    Allow user to enter his own IP and Port

--- server.py
+++ server.py
@@ -4,15 +4,18 @@ from encodings import utf_8
 # This variable becomes a socket object configured to use IPV4 addresses and TCP protocol
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# These two variables configure the IP address and port that the server will listen on.
-listen_on = "127.0.0.1"
-port_number = 9000
+
+# Enter the IP address that the machine has
+enter_ip = input("Enter the IP for the server to listen on: ")
+enter_port = int(input("Enter the port number for the server: "))
+
 
 # Show that the server is listening on what ip and port number
-print(f"Server listening on IP: {listen_on} Port: {port_number}")
+print(f"Server listening on IP: {enter_ip} Port: {enter_port}")
 
 # This binds the IP address and the port number to the server
-server.bind((listen_on, port_number))
+server.bind((enter_ip, enter_port))
+
 
 # This allows the server to wait for the connections, with a maximum of 5 clients in a que
 server.listen(5)
